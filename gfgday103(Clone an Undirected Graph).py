5efe2e245bde91d4d83f116b6a9439578e18de47
Aditya Pandey <adityapandey0502@gmail.com>
2025-04-13 13:11:59 +0530

    Day 103 gfg daily april 13th

---
+++ gfgday103(Clone an Undirected Graph).py
@@ -0,0 +1,12 @@
+class Solution():
+    def cloneGraph(self, node):
+        seen = {}
+        def clone(n):
+            if n in seen:
+                return seen[n]
+            cp = Node(n.val, [])
+            seen[n] = cp
+            for nbr in n.neighbors:
+                cp.neighbors.append(clone(nbr))
+            return cp
+        return clone(node)
